9ab7e59a4e6df9c85686162944d287dab996941d
Kaio <luizkaio9815@gmail.com>
2021-11-02 03:17:09 -0300

    Cria as Rotas de Navegação das Páginas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,10 +12,10 @@ export const Header = ()=> {
                 <img src={gif} alt="Logo-name" />
             </div>
             <nav className='nav-bar'>   
-                <Link to="">HOME</Link>
-                <Link to="">PERSONAGENS</Link>
-                <Link to="">SOBRE A SÉRIE</Link>
-                <Link to="">DETALHES DOS PERSONAGENS</Link>
+                <Link to="/">HOME</Link>
+                <Link to="/Personagens">PERSONAGENS</Link>
+                <Link to="/SobreASerie">SOBRE A SÉRIE</Link>
+                <Link to="/DetalhesDePersonagens">DETALHES DOS PERSONAGENS</Link>
             </nav>
             <button className='responsive-btn'>▽</button>
         </header>
