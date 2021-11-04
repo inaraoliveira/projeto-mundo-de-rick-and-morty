8247789d2da4a53aa907ae45b52c03ef74a71e15
Kaio <luizkaio9815@gmail.com>
2021-11-04 18:04:06 -0300

    Coloca o App.css na pasta stayles e adiciona a logo png

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import './styles/App.css'
 import { Header } from './components/Header';
+import { Rotas } from './Routes';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Rotas />
     </div>
   );
 }
