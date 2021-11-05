10803b0de89d9920ed7b293df01771824940a7a4
Inara Oliveira <inaraolliveira23@hotmail.com>
2021-11-04 21:48:49 -0300

    Cria texto sobre a série

---
+++ src/components/SobreSerie.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export const SobreSerie = () => {
+    return(
+        <section>
+            <div className='nav'>
+                <h4>Sobre a série</h4>
+                <h4>Galeria de fotos</h4>
+                <div className='conteudo'>
+                    Rick and Morty é uma série animada de comédia e ficção científica
+                    criada por Justin Roiland e Dan Harmon que mostra as aventuras
+                    interdimensionais de Rick, um cientista alcoólatra com a mente
+                    mais brilhante da galáxia, e seu neto Morty. Rick, apesar de ótimo
+                    cientista, é péssimo quando o assunto é familia e evita qualquer
+                    tipo de relação familiar levando seu neto em todas as suas
+                    aventuras. Já Morty é um garoto comum, um tanto que covarde, que 
+                    se vê sendo arrastado para as confusões de seu avô. As aventuras
+                    ocorrem em um número infinito de realidades, com os personagens
+                    viajando para outros planetas e dimensões através de portais. Há
+                    diferentes versões dos personagens habitando em outras dimensões
+                    em todo o multiverso, tendo suas características variadasde uma
+                    realidade para outra. E além dos protagonistas, a série traz outros
+                    personagens como Summer, irmã de Morty; Beth, filha de Rick e mãe
+                    de Morty; e Jerry, o pai de Morty.
+                </div>
+            </div>
+        </section>
+    )
+}
