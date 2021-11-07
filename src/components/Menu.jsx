c15c6bb50661404df2697a6d09e5f82995dcd2da
Kaio <luizkaio9815@gmail.com>
2021-11-07 20:08:21 -0300

    Adiciona o Componente Menu do Header

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,22 +1,24 @@
-import React, { useRef, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import '../styles/Menu.css';
 
 export const Menu = ()=> {
-    const menuRef = useRef(null);
-    const [isActive, setIsActive] = useState(false);
-    const onClick = () => setIsActive(!isActive);
+    const [clicked, setclicked] = useState(false);
+    const toggle = ()=> setclicked(!clicked);
 
-    console.log(isActive)
+    
+
     return(
         <div className='dropdown'>
-            <button onClick={onClick} className='responsive-btn'>▽</button>
-            <div ref={menuRef} className='dropdown-menu' >
+            <button onClick={()=>{toggle(!clicked)}} className='responsive-btn'>▽</button>
+            { clicked ? 
+            <div className='dropdown-menu'>   
                 <Link to="/">HOME</Link>
                 <Link to="/personagens">PERSONAGENS</Link>
                 <Link to="/sobreaserie">SOBRE A SÉRIE</Link>
                 <Link to="/localizacao">LOCALIZAÇÃO</Link>
             </div>
+            :''}
         </div>
     );
 }
