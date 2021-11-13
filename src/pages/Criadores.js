330152472bbe44e0ac458d221654504177a1d370
miihfranca <michelefdf@gmail.com>
2021-11-12 22:50:22 -0300

    cria conteudo da página de criadores

--- src/pages/Criadores.js
+++ src/pages/Criadores.js
@@ -1,6 +1,16 @@
 import React, { useEffect } from "react";
-import { Perfil } from "../components/Perfil";
+// import { Perfil } from "../components/Perfil";
 import '../styles/Criadores.css'
+
+import Emanuel from "../assets/Perfil/isack.png"
+import Kaio from "../assets/Perfil/kaio.png"
+import Marcelo from "../assets/Perfil/marcelo.png"
+import Nara from "../assets/Perfil/nara.png"
+import Michele from "../assets/Perfil/michele.png"
+import Git from "../assets/Perfil/Git.png"
+import Linkedin from "../assets/Perfil/Linkedin.png"
+import Gmail from "../assets/Perfil/Gmail.png"
+import Thassio from "../assets/Perfil/thassio.jpeg"
 
 export const Criadores =()=> {
 
@@ -8,17 +18,73 @@ export const Criadores =()=> {
         document.title = 'Criadores'
     }, []);
     
-    return(
-        <div>
-            <section className='container-perfis'>
-                <Perfil imagePerfil={()=>{}} imageDis='' criador='Michele' />
-                <Perfil imagePerfil={()=>{}} imageDis='' criador='Isaac' />
-                <Perfil imagePerfil={()=>{}} imageDis='' criador='Inara' />
-                <Perfil imagePerfil={()=>{}} imageDis='' criador='Thassio' />
-                <Perfil imagePerfil={()=>{}} imageDis='' criador='Marcelo' />
-                <Perfil imagePerfil={()=>{}} imageDis='' criador='Kaio' />
-            </section>
-        </div>
+   
+  return (
+    <section>
+      <div className="imagem">
+          
+        <Card
+          imagem={Emanuel}
+          title="EMANUEL" 
+          imgGit={Git}
+          imgLinkedin={Linkedin}
+          imgGmail={Gmail}
+        />
 
-    );
+        <Card
+          imagem={Marcelo}
+          title="MARCELO"
+          imgGit={Git}
+          imgLinkedin={Linkedin}
+          imgGmail={Gmail}
+        />
+
+        <Card
+          imagem={Thassio}
+          title="THASSIO"
+          imgGit={Git}
+          imgLinkedin={Linkedin}
+          imgGmail={Gmail}
+        />
+
+      <Card
+          imagem={Nara}
+          title="NARA"
+          imgGit={Git}
+          imgLinkedin={Linkedin}
+          imgGmail={Gmail}
+        />
+          <Card
+                    imagem={Kaio}
+                    title="KAIO"
+                    imgGit={Git}
+                    imgLinkedin={Linkedin}
+                    imgGmail={Gmail}
+                  />
+        <Card
+          imagem={Michele}
+          title="MICHELE"
+          imgGit={Git}
+          imgLinkedin={Linkedin}
+          imgGmail={Gmail}
+        />
+      </div>
+    </section>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="card_perfil">
+      <img src={props.imagem} className="card_imagem"alt=""/>
+      <div className="card_body">
+        <h2 className="card_title">{props.title}</h2>
+      </div>
+      <div className="icon" > 
+        <a><img className="git" src={props.imgGit}alt=""/></a>
+        <a><img className="linkedin" src={props.imgLinkedin}alt=""/></a>
+        <a><img className="gmail" src={props.imgGmail} alt=""/></a>
+      </div>
+    </div>
+  );
 }
