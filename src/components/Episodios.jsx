78c141636a56947ab7eecf424e9695bea15841a3
Kaio <luizkaio9815@gmail.com>
2021-11-15 22:51:57 -0300

    Adiciona um resumo nos Recomendados e altera o css

--- src/components/Episodios.jsx
+++ src/components/Episodios.jsx
@@ -3,7 +3,8 @@ import React from "react";
 export const Episodios = (eps)=>{
     return(
         <div className="container-ep" >
-            <h3>{eps.texto}</h3>
+            <h3>{eps.episodio}</h3>
+            <p>{eps.resumo}</p>
         </div>
     );
 }
