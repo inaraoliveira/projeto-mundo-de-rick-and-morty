59446852430c18393580cdae123007881394dcbc
Kaio <luizkaio9815@gmail.com>
2021-11-16 23:01:38 -0300

    Retorta o click do menu ao que estava antes

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,14 +3,14 @@ import { NavLink } from "react-router-dom";
 import '../styles/Menu.css';
 
 export const Menu = ()=> {
-    const [clicked, setclicked] = useState(true);
+    const [clicked, setclicked] = useState(false);
     const toggle = ()=> setclicked(!clicked);
 
     
 
     return(
         <div className='dropdown'>
-            <button onTouchStart={()=>setclicked(!clicked)} onClick={()=>{toggle(!clicked)}} className='responsive-btn'>▽</button>
+            <button onClick={()=>{toggle(!clicked)}} className='responsive-btn'>▽</button>
             { clicked ? 
             <div className='dropdown-menu'>   
                 <NavLink activeStyle={{color: 'chartreuse'}} exact to="/">HOME</NavLink>
